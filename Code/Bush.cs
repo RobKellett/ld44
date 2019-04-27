1de4a71c75ed86c294a4d0acffe589e612fc35ac
Rob Kellett <rob@kelle.tt>
2019-04-27 15:12:43 -0400

    Bushes

--- Code/Bush.cs
+++ Code/Bush.cs
@@ -1,6 +1,20 @@
 using Godot;
 using System;
+using LD44.Utilities;
 
 public class Bush : BasePlant
 {
+  private Texture noFruitTexture;
+  private Texture fruitTexture;
+
+  public override void _Ready()
+  {
+    noFruitTexture = (Texture)GD.Load("res://Assets/bush.png");
+    var textureIdx = RNG.Instance.Next(3);
+    fruitTexture = (Texture)GD.Load($"res://Assets/bush-fruit{textureIdx}.png");
+
+    var sprite = GetChild<ResourceSprite>(0);
+    sprite.SetTexture(noFruitTexture);
+    sprite.RandomizePosition();
+  }
 }
