4206762b69b83f893e7ba7f20aebf866e37fc6e0
Pi Lanningham <pi.lanningham@gmail.com>
2019-04-27 15:58:06 -0400

    More typos...

--- Code/Tree.cs
+++ Code/Tree.cs
@@ -20,7 +20,12 @@ public class Tree : ForestedPlant
     _sprite = GetChild<ResourceSprite>(0);
     _sprite.SetTexture(_smallTreeTexture);
     _sprite.RandomizePosition();
-;
+    // Stagger all tree growth uniformly across the range defined by GROWTH_TIMER so that things are more organic
+    // Also make sure that everything "grows" the first time, so we have correct adult/child trees
+    GROWTH_TIMER = 15f;
+    _timeSinceGrowth = (float)(RNG.Instance.NextDouble() + 1) * GROWTH_TIMER;
+    GROWTH_PROBABILITY = 0.3f;
+    type = PlantType.Tree;
   }
 
   public override void _Process(float delta) {
