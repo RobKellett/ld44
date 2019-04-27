a6447db3733d6e81891124ec643d7d55a940a643
Rob Kellett <rob@kelle.tt>
2019-04-26 21:43:26 -0400

    Add type-safe group utility

---
+++ Code/Utilities/Group.cs
@@ -0,0 +1,43 @@
+using System;
+using Godot;
+
+namespace LD44.Utilities
+{
+  public class Group<TGroupMember>
+  {
+    public string Name { get; }
+
+    internal Group(string name)
+    {
+      Name = name;
+    }
+
+    public void Add<T>(T newMember) where T : Node, TGroupMember
+    {
+      newMember.AddToGroup(Name);
+    }
+
+    public void Call(SceneTree tree, Action<TGroupMember> action)
+    {
+      var nodes = tree.GetNodesInGroup(Name);
+      foreach (var node in nodes)
+      {
+        if (node is TGroupMember castedNode)
+        {
+          action(castedNode);
+        }
+      }
+    }
+  }
+
+  public static class Group
+  {
+    // To define a group:
+    // public static Group<IGroupMember> GroupName = new Group<IGroupMember>("GroupName");
+
+    public static void CallGroup<T>(this SceneTree tree, Group<T> group, Action<T> action)
+    {
+      group.Call(tree, action);
+    }
+  }
+}
