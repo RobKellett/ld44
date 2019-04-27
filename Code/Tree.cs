0b99aa8ce94b40b762d740c2b3cb5b884f061a2e
Rob Kellett <rob@kelle.tt>
2019-04-27 18:15:35 -0400

    Animate tree growth

--- Code/Tree.cs
+++ Code/Tree.cs
@@ -9,9 +9,7 @@ public interface ICareAboutMapUpdates {
 public class Tree : ForestedPlant, ICareAboutMapUpdates
 { 
   private bool _grown = false;
-  private ResourceSprite _sprite;
-  private Texture _smallTreeTexture;
-  private Texture _bigTreeTexture;
+  private AnimationPlayer _animationPlayer;
 
   // Called when the node enters the scene tree for the first time.
   public override void _Ready()
@@ -20,12 +18,20 @@ public class Tree : ForestedPlant, ICareAboutMapUpdates
     Group.MapUpdates.Add(this);
 
     var textureIdx = RNG.Instance.Next(2);
-    _smallTreeTexture = (Texture)GD.Load($"res://Assets/tree-small{textureIdx}.png");
-    _bigTreeTexture = (Texture)GD.Load($"res://Assets/tree{textureIdx}.png");
+    var smallTreeTexture = (Texture)GD.Load($"res://Assets/tree-small{textureIdx}.png");
+    var bigTreeTexture = (Texture)GD.Load($"res://Assets/tree{textureIdx}.png");
 
-    _sprite = GetChild<ResourceSprite>(0);
-    _sprite.SetTexture(_smallTreeTexture);
-    _sprite.RandomizePosition();
+    var smallSprite = GetChild<ResourceSprite>(0);
+    var bigSprite = GetChild<Sprite>(1);
+    smallSprite.SetTexture(smallTreeTexture);
+    bigSprite.SetTexture(bigTreeTexture);
+    smallSprite.RandomizePosition();
+    bigSprite.Position = smallSprite.Position;
+    bigSprite.ZIndex = smallSprite.ZIndex;
+
+    _animationPlayer = GetChild<AnimationPlayer>(2);
+    _animationPlayer.Play("Spawn");
+
     // Stagger all tree growth uniformly across the range defined by GROWTH_TIMER so that things are more organic
     // Also make sure that everything "grows" the first time, so we have correct adult/child trees
     GROWTH_TIMER = 15f;
@@ -66,7 +72,7 @@ public class Tree : ForestedPlant, ICareAboutMapUpdates
     base._Process(delta);
     if(_asleep && !_grown) {
       _grown = true;
-      _sprite.SetTexture(_bigTreeTexture);
+      _animationPlayer.Play("Grow");
     }
   }
 }
